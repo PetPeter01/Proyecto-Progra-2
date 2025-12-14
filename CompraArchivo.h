f749b69364061c39e38e53fe722b9ab5282ad950
Peter <losantopeter@gmail.com>
2025-12-14 18:56:09 -0300

    clase encargada de manejar los archivos de compra

---
+++ CompraArchivo.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <cstring>
+#include "Compra.h"
+
+class CompraArchivo {
+private:
+    char _nombreArchivo[30];
+    int  tamanioRegistro;
+
+public:
+    CompraArchivo(const char* nombreArchivo = "compras.dat") {
+        std::strcpy(_nombreArchivo, nombreArchivo);
+        tamanioRegistro = sizeof(Compra);
+    }
+
+    int   altaCompra();
+    bool  agregarRegistro(Compra reg);
+    Compra leerRegistro(int pos);
+    int   contarRegistros();
+    bool  listarRegistros();
+    int   generarIdCompra();
+    int   ModificarRegistro(Compra reg, int pos);
+    bool  bajaLogica(int idCompra);
+    int   buscarPorId(int idCompra);
+    int   reactivarCompra(int idCompra);
+};
