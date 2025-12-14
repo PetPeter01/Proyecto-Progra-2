98c4d71df76bfba5cc6a78b338df2401b6c7a85a
Peter <losantopeter@gmail.com>
2025-12-14 18:59:21 -0300

    clase encargada de manejar los archivos de DetalleCompra

---
+++ DetalleCompraArchivo.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstring>
+#include "DetalleCompra.h"
+
+class DetalleCompraArchivo {
+private:
+    char _nombreArchivo[30];
+    int  tamanioRegistro;
+
+public:
+    DetalleCompraArchivo(const char* nombreArchivo = "detallesCompras.dat") {
+        std::strcpy(_nombreArchivo, nombreArchivo);
+        tamanioRegistro = sizeof(DetalleCompra);
+    }
+
+    float altaDetalle(int idCompra);
+    int   agregarRegistro(DetalleCompra reg);
+    int   listarPorCompra(int idCompra);
+    float totalPorCompra(int idCompra);
+};
